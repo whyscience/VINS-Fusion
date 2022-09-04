39fb03de7c3e78be4167c3485399d98296cfe555
jiabin.fan <jiabin.fan@clobotics.com>
2022-09-04 16:24:52 +0800

    fix for Ubuntu 20.04
    # Conflicts:
    #	global_fusion/.gitignore

--- vins_estimator/src/featureTracker/feature_tracker.cpp
+++ vins_estimator/src/featureTracker/feature_tracker.cpp
@@ -10,6 +10,8 @@
  *******************************************************/
 
 #include "feature_tracker.h"
+#include <opencv2/imgproc/types_c.h>
+
 
 bool FeatureTracker::inBorder(const cv::Point2f &pt)
 {
